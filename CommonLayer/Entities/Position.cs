9390d3705bb5cd80611c45471601b6ceb81d7667
androso <androso421@gmail.com>
2024-10-05 15:04:55 -0600

    init common layer

---
+++ CommonLayer/Entities/Position.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer.Entities
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string PositionName { get; set; }
+        public int BaseSalary { get; set; }
+        public int HierarchyLevel { get; set; } 
+    }
+}
