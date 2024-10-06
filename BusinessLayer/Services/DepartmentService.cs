403d243f4a8e4f8a475779f0569efd050cea6350
androso <androso421@gmail.com>
2024-10-05 19:57:26 -0600

    adding the department service provider

---
+++ BusinessLayer/Services/DepartmentService.cs
@@ -0,0 +1,40 @@
+﻿using CommonLayer.Entities;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class DepartmentService
+    {
+        private DepartmentRepository _departmentRepository;
+
+        public DepartmentService()
+        {
+            _departmentRepository = new DepartmentRepository();
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            return _departmentRepository.GetDepartments();
+        }
+
+        public void AddDepartment(Department department)
+        {
+            _departmentRepository.AddDepartment(department);
+        }
+
+        public void UpdateDepartment(Department department)
+        {
+            _departmentRepository.EditDepartment(department);
+        }
+
+        public void DeleteDepartment(int id)
+        {
+            _departmentRepository.DeleteDepartment(id);
+        }
+    }
+}
