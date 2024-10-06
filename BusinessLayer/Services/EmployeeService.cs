37c77b05a283b4adc644bc1a1d2c41feb1e75257
androso <androso421@gmail.com>
2024-10-05 20:20:50 -0600

    adding initial service for employee

---
+++ BusinessLayer/Services/EmployeeService.cs
@@ -0,0 +1,50 @@
+﻿using CommonLayer.Entities;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class EmployeeService
+    {
+        private EmployeeRepository _employeeRepository;
+
+        public EmployeeService()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return _employeeRepository.GetEmployees();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee.HireDate > DateTime.Now)
+            {
+                throw new ArgumentException("Fecha de contratación no puede ser en el futuro");
+            }
+
+            _employeeRepository.AddEmployee(employee);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            if (employee.HireDate > DateTime.Now)
+            {
+                throw new ArgumentException("Fecha de contratación no puede ser en el futuro");
+            }
+
+            _employeeRepository.EditEmployee(employee);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            _employeeRepository.DeleteEmployee(id);
+        }
+    }
+}
