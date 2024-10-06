2c36d7368467fec57cb28725c9ec07b070ae41d5
androso <androso421@gmail.com>
2024-10-05 20:07:30 -0600

    adding position service

---
+++ BusinessLayer/Services/PositionService.cs
@@ -0,0 +1,40 @@
+﻿using CommonLayer.Entities;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class PositionService
+    {
+        private PositionRepository _positionRepository;
+
+        public PositionService()
+        {
+            _positionRepository = new PositionRepository();
+        }
+
+        public List<Position> GetAllPositions()
+        {
+            return _positionRepository.GetPositions();
+        }
+
+        public void AddPosition(Position position)
+        {
+            _positionRepository.AddPosition(position);
+        }
+
+        public void UpdatePosition(Position position)
+        {
+            _positionRepository.EditPosition(position);
+        }
+
+        public void DeletePosition(int id)
+        {
+            _positionRepository.DeletePosition(id);
+        }
+    }
+}
