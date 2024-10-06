d8c2d7353de0f93d9d45a505f4b4e7ea05e88fda
androso <androso421@gmail.com>
2024-10-05 18:55:46 -0600

    updating the commonlayer for department and Employee

--- CommonLayer/Entities/Employee.cs
+++ CommonLayer/Entities/Employee.cs
@@ -11,9 +11,9 @@ namespace CommonLayer.Entities
     {
         public int Id { get; set; }
         public string FullName  { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public DateTime HireDate { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
         public int PositionId { get; set; }
     
     }
